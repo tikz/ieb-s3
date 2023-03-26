b3e703eade0e10b684c049a788c4b1b3ade1bc8a
Mauro Song <maurosong@protonmail.com>
2023-03-26 13:41:21 -0300

    Agregar model Product y migracion con dos productos de ejemplo

---
+++ products/migrations/0002_populate.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+
+from django.db import migrations, transaction
+
+# Poblar tabla con 2 productos de ejemplo
+
+def populate(apps, schema_editor):
+    Product = apps.get_model('products', 'Product')
+
+    with transaction.atomic():
+        Product.objects.create(code="R129", cost=Decimal(2200), price=Decimal(6000), description="Remera de algodón puro hipoalergénico.")
+        Product.objects.create(code="F600-L-X", cost=Decimal(5000), price=Decimal(19000), description="Llanta original de Fiat 600, en buen estado.")
+
+
+def clear(apps, schema_editor):
+    Product = apps.get_model('products', 'Product')
+
+    with transaction.atomic():
+        Product.objects.filter(code="R129").delete()
+        Product.objects.filter(code="F600-L-X").delete()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('products', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate, clear),
+    ]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    # Código de producto o ID (texto de longitud variable)
+    code = models.CharField(max_length=200) 
+
+    # Precio de compra (número real)
+    cost = models.DecimalField(decimal_places=2, max_digits=10)
+
+    # Precio de venta al público (número real)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    # Descripción (texto de longitud variable)
+    description = models.TextField()
