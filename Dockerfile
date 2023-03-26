8596adc66c98c2040d4d15896c6fee16e17bb934
Mauro Song <maurosong@protonmail.com>
2023-03-26 18:34:43 -0300

    Agregar Dockerfile. Mover actualizacion de precios a django admin script.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10-alpine
+
+RUN apk add bash
+
+RUN mkdir -p /iebs3
+COPY . /iebs3/
+WORKDIR /iebs3
+
+RUN pip install pipenv && pipenv install --dev --system --deploy
+
+EXPOSE 80
+CMD trap 'killall python; killall -s KILL gunicorn' INT && python manage.py update_prices & gunicorn --chdir iebs3 --bind :80 iebs3.wsgi:application
--- api/management/commands/update_prices.py
+++ api/management/commands/update_prices.py
@@ -1,23 +1,24 @@
-import logging
 import random
-import threading
 import time
 from decimal import Decimal
+
+from django.core.management.base import BaseCommand, CommandError
 
 from api.models import Product
 
 
-class PriceUpdaterThread(threading.Thread):
-    def run(self):
+class Command(BaseCommand):
+    help = 'Actualiza los precios de todos los productos (al azar)'
+
+    def handle(self, *args, **options):
         while True:
             for product in Product.objects.all():
-                multiplier = Decimal(1 + random.randint(-50, 50)/500)
+                multiplier = Decimal(1 + random.randint(-5, 5)/100)
                 product.cost = product.cost * multiplier
                 product.price = product.price * multiplier
                 product.save()
 
                 multiplier_format = f"{(1-multiplier)*100:+.2f}"
                 new_prices = f"cost={product.cost:.2f} price={product.price:.2f}"
-                logging.info(f"Actualizado {product.code: <10} {multiplier_format}% \t {new_prices}")
-
-            time.sleep(random.randint(1, 10))
+                self.stderr.write(self.style.SUCCESS(f"Actualizado {product.code: <10} {multiplier_format}% \t {new_prices}"))
+            time.sleep(random.randint(1,5))
